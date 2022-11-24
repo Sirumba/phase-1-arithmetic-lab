d77befc22c752050d9960ab16fe4536523023caa
Sirumba <simonsmaina45@gmail.com>
2022-11-24 15:24:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log (multiply)
+
+let random;
+random = Math.floor(Math.random() + 1);
+
+let num3 = 128;
+let num4 = 32;
+const mod = num3/num4;
+
+let max = Math.max(3, 8, 19, 13, 20);
